e9519fc5fece3fa313ba5033f6a3177c1feee5e4
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-11-14 14:43:14 +0530

    all testcases passed

--- src/components/QuestionBox.jsx
+++ src/components/QuestionBox.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import QuestionChoice from './QuestionChoice';
 
 const QuestionBox = ({ questions, query, selectchoice }) => {
+  const [highlighted, sethighlighted] = useState(false);
+  const textRef = useRef();
   // TASK 3: Implement logic to highlight the question text when the "Highlight" button is clicked.
   // You will need to manage state for this. Make the text red when highlighted and blue when not.
   const highlightHandler = () => {
     // Implement the highlight logic
+    sethighlighted(prev=>!prev);
+    textRef.current.style.color = (highlighted)?"blue":"red";
   };
 
   const removeHighlightHandler = () => {
     // Implement the remove highlight logic
+    textRef.current.style.color = 'black';
   };
 
   return (
@@ -22,7 +27,7 @@ const QuestionBox = ({ questions, query, selectchoice }) => {
           Question: {query + 1} out of {questions.length}
         </h1>
         {/* TASK 3: Change color of this text based on highlight */}
-        <h2>{questions[query].text}</h2>
+        <h2 ref={textRef}>{questions[query].text}</h2>
 
         <QuestionChoice
           options={questions[query].options}
